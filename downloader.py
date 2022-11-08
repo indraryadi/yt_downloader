006d65d3018c1c647de89c8f8f7ba12b01527602
indraryadi <indraaryadikadek@gmail.com>
2022-11-09 07:54:58 +0800

    fix download format

--- downloader.py
+++ downloader.py
@@ -6,18 +6,25 @@ import os
 
 def download(save_path,selected,download_type):
     if download_type=='1':
-        with Bar('Processing',max=100) as bar:
-            for i in range(100):
-                selected.download(output_path=save_path)
-                sleep(0.02)
-                bar.next()
-        bar.finish()
+        selected.download(output_path=save_path)
+        # with Bar('Processing',max=100) as bar:
+        #     for i in range(100):
+        #         selected.download(output_path=save_path)
+        #         sleep(0.02)
+        #         bar.next()
+        # bar.finish()
     elif download_type=='2':
-        with Bar('Processing',max=100) as bar:
-            for i in range(100):
-                out_file=selected.download(output_path=save_path)
-                base, ext = os.path.splitext(out_file)
-                new_file = base + '.mp3'
-                os.rename(out_file, new_file)
-                sleep(0.02)
-                bar.next()
+        file_name=selected['title']+".mp3"
+        # print(file_name)
+        out_file=selected['stream'].download(output_path=save_path,filename=file_name)
+        # base, ext = os.path.splitext(out_file)
+        # new_file = base + '.mp3'
+        # os.rename(out_file, new_file)
+        # with Bar('Processing',max=100) as bar:
+        #     for i in range(100):
+        #         out_file=selected.download(output_path=save_path)
+        #         base, ext = os.path.splitext(out_file)
+        #         new_file = base + '.mp3'
+        #         os.rename(out_file, new_file)
+        #         sleep(0.02)
+        #         bar.next()
