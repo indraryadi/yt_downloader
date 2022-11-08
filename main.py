06f57ef8396930dec7caa340891cf8ac233565b9
indraryadi <indraaryadikadek@gmail.com>
2022-11-09 07:56:31 +0800

    add process time

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from video import *
 from downloader import *
 from mp3 import *
+import time
 
 save_path=str(Path.home()/'Downloads')
 link = input("enter the link:")
@@ -13,13 +14,27 @@ if download_type=='1':
   v_select=get_video(link)
   # print(v_select)
   try:
+    print("downloading video ...")
+    t1=time.time()
     download(save_path,v_select,download_type)
+    t2=time.time()
+    tot_time=t2-t1
+    tot_time=time.strftime("%H:%M:%S", time.gmtime(tot_time))
+    print(f"download success, finished in: {tot_time}")
   except:
     print('failed download')
 elif download_type=='2':
   v_select=get_mp3(link)
   try:
+    print("downloading mp3 ...")
+    t1=time.time()
     download(save_path,v_select,download_type)
+    t2=time.time()
+    tot_time=t2-t1
+    tot_time=time.strftime("%H:%M:%S", time.gmtime(tot_time))
+    print(f"download success, finished in: {tot_time}")
+
+    print("download success")
   except:
     print('failed download')
 else:
