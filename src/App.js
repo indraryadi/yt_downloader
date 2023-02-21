e04931fd895f25a3fda16dc28216e6096ad2cc9d
indraryadi <indraaryadikadek@gmail.com>
2023-02-22 07:38:28 +0800

    add radio button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import ButtonProcess from './components/ButtonProcess';
 import InputLink from './components/InputLink';
+import RadioButton from './components/RadioButton';
 function App() {
   return (
     <>
       <div className='screen'>
         <div className='container'>
-          <div className='user-input'>
-            <InputLink/>
+          <div className='user-input-container'>
+            <div className='user-input'>
+              <div className='input'>
+                <InputLink/>
+              </div>
+              <div className='radio-button'>
+                <RadioButton/>
+              </div>
+            </div>
             <ButtonProcess/>
           </div>
           <div className='video-title'></div>
---
+++ src/components/RadioButton.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const RadioButton = ()=>{
+    return(
+        <>
+            <div>
+                <input type="radio"/>
+                <label>a</label>
+            </div>
+            <div>
+                <input type="radio"/>
+                <label>a</label>
+            </div>
+        </>
+    )
+}
+
+export default RadioButton
