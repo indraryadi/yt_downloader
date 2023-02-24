f410eb1393233ca289924ae263f7153071f3739f
indra appkey <indra.appkey@gmail.com>
2023-02-24 15:25:13 +0800

    fetch the quality from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import axios from 'axios';
 import './App.css';
 import ButtonProcess from './components/ButtonProcess';
 import InputLink from './components/InputLink';
@@ -7,13 +8,35 @@ function App() {
 
   const [link,setLink]= useState("")
   const [downloadType,setDownloadType]=useState("mp3")
-  console.log("re-render")
-  console.log(link)
+  const [quality,setQuality]=useState({})
+  // console.log("re-render")
+  // console.log(link)
 
-  const handleSubmit=(e)=>{
+  const baseUrl="http://localhost:5000/api/"
+  const instanceApi=axios.create({
+    "baseURL":baseUrl
+  })
+
+
+
+  const handleSubmit= async (e)=>{
     e.preventDefault()
-    
-    console.log(`${link} ${downloadType}`)
+    const header={
+      "Content-Type": "multipart/form-data"
+    }
+    const data=new FormData()
+    data.append('url',link)
+    data.append('download_type',downloadType)
+    console.log(data)
+    try {
+      const response = await instanceApi.post('getQuality',data,{header})
+
+      setQuality(response.data)
+    } catch (error) {
+      console.log(error)
+    }    
+    // console.log(`${link} ${downloadType}`)
+    console.log(quality)
   }
 
   return (
@@ -23,8 +46,8 @@ function App() {
           <div className='input-form'>
             <form onSubmit={handleSubmit}>
               <div className='user-input-container'>
-                <InputLink name="link" setLink={setLink}/> 
-                <RadioButton name="download_type" setVideoType={setDownloadType} downloadType={downloadType}/>
+                <InputLink name="url" setLink={setLink}/> 
+                <RadioButton name="download_type" setDownloadType={setDownloadType} downloadType={downloadType}/>
               </div>
               <div className='button-container'>
                 <ButtonProcess/>
