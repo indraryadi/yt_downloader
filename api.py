147aea538efe5e75855393384c2e4bab58f75939
indra appkey <indra.appkey@gmail.com>
2023-02-25 09:47:00 +0800

    edit core/mp3 so it can used to download mp4

--- api.py
+++ api.py
@@ -16,36 +16,59 @@ def test():
     data={"messsage":"hello"}
     return jsonify(data)
 
+# @app.route("/api/getQuality",methods=["POST"])
+# def getQuality():
+#     url=request.form["url"]
+#     download_type=request.form["download_type"]
+
+#     if download_type == "mp3":
+#         raw_data=get_video_stream(url)
+#         video_quality=get_video_quality(raw_data)
+#         # send quality data to fe to display the list
+#         # get the key (itag)
+
+#         #for download neet to call function get stream again bc stream can convert to string and vice versa using io but the type need to be pytube.stream to download it but using io become _io.StringIO
+
+
+#         # return {"quality":video_quality}
+#         response= jsonify(video_quality) 
+#         # response.headers.add('Access-Control-Allow-Origin', '*')
+#         # response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+#         # response.headers.add('Access-Control-Allow-Methods', 'POST')
+#         return response 
+
 @app.route("/api/getQuality",methods=["POST"])
 def getQuality():
     url=request.form["url"]
     download_type=request.form["download_type"]
 
-    if download_type == "mp3":
-        raw_data=get_video_stream(url)
-        video_quality=get_video_quality(raw_data)
-        # send quality data to fe to display the list
-        # get the key (itag)
-
-        #for download neet to call function get stream again bc stream can convert to string and vice versa using io but the type need to be pytube.stream to download it but using io become _io.StringIO
-
-
-        # return {"quality":video_quality}
-        response= jsonify(video_quality) 
-        # response.headers.add('Access-Control-Allow-Origin', '*')
-        # response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-        # response.headers.add('Access-Control-Allow-Methods', 'POST')
-        return response 
+    raw_data=get_video_stream(url)
+    video_quality=get_video_quality(raw_data,download_type)
+    response= jsonify(video_quality) 
+    # response.headers.add('Access-Control-Allow-Origin', '*')
+    # response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    # response.headers.add('Access-Control-Allow-Methods', 'POST')
+    return response
 
 @app.route("/api/downloadVideo",methods=["POST"])
 def downloadVideo():
     url=request.form["url"]
+    download_type=request.form["download_type"]
+
     raw_data=get_video_stream(url)
     quality=request.form["qualities"]
+
     link=get_mp3_link(raw_data["stream"],quality)
-    # print(raw_data["yt"].title)
-    # print(type(link))
-    download(_save_path,link,"mp3",raw_data["yt"].title)
+
+    print(f'{url} {download_type}')
+    # download(_save_path,link,"mp3",raw_data["yt"].title)
+    try:
+        print(f'try download {raw_data["yt"].title}.{download_type}')
+        download(_save_path,link,download_type,raw_data["yt"].title)
+        print('SUCCESS DOWNLOAD!!!!!')
+    except Exception as e:
+        print("FAILED DOWNLOAD")
+        # print(e)
 
     return {"message":"ok"}
 
--- core/mp3.py
+++ core/mp3.py
@@ -11,14 +11,39 @@ def get_video_stream(url):
     except Exception as e:
         print(e)
 
-def get_video_quality(stream):
-    try:
-        _raw_data=stream["stream"].filter(only_audio=True,mime_type="audio/mp4")
-        _quality={}
-        for i in _raw_data:
-            _quality[i.itag]=i.abr
+# def get_video_quality(stream):
+#     try:
+#         _raw_data=stream["stream"].filter(only_audio=True,mime_type="audio/mp4")
+#         _quality={}
+#         for i in _raw_data:
+#             _quality[i.itag]=i.abr
 
-        return {"title":stream["yt"].title,"qualities":_quality}
+#         return {"title":stream["yt"].title,"qualities":_quality}
+
+#     except Exception as e:
+#         print(e)
+
+def get_video_quality(stream,download_type):
+    try:
+        if download_type == "mp3":
+            _raw_data=stream["stream"].filter(only_audio=True,mime_type="audio/mp4")
+            _quality={}
+
+            for i in _raw_data:
+                _quality[i.itag]=i.abr
+
+            return {"title":stream["yt"].title,"qualities":_quality}
+
+        elif download_type == "mp4":
+            _raw_data=stream["stream"].filter(progressive=True)
+            _quality={}
+
+            for i in _raw_data:
+                _quality[i.itag]=i.abr
+
+            return {"title":stream["yt"].title,"qualities":_quality}
+
+
 
     except Exception as e:
         print(e)
@@ -31,8 +56,8 @@ def get_mp3_link(stream,selected_quality):
     except:
         print("connection error")
 
-if __name__=="__main__":
-    yt=get_video_stream("https://www.youtube.com/watch?v=OTBdO18zmxQ")
-    quality=get_video_quality(yt)
-    link=get_mp3_link(yt["stream"],139)
-    print(link)
+# if __name__=="__main__":
+#     yt=get_video_stream("https://www.youtube.com/watch?v=OTBdO18zmxQ")
+#     quality=get_video_quality(yt)
+#     link=get_mp3_link(yt["stream"],139)
+#     print(link)
